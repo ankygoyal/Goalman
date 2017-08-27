4fe492b00af5f87d34477f6eba24e262e2b4df3c
technophilia <technophilia@VS2015>
2017-08-27 16:45:39 +0000

    Clearing text box once request is procesed

--- GoalmanUI/SpeechBot.js
+++ GoalmanUI/SpeechBot.js
@@ -84,15 +84,33 @@ function send() {
         headers: {
             "Authorization": "Bearer " + accessToken
         },
-        data: JSON.stringify({ query: text, lang: "en", sessionId: "f9d5e3dc-2046-4f40-b092-6ae8abc0ca93" }),
+        data: JSON.stringify({ query: text, lang: "en", sessionId: guid() }),
         success: function (data) {
             prepareResponse(data);
+            clearInput();
         },
         error: function () {
             respond(messageInternalError);
+            clearInput();
         }
     });
 }
+
+function clearInput()
+{
+    $speechInput.val("");
+}
+
+function guid() {
+    function s4() {
+        return Math.floor((1 + Math.random()) * 0x10000)
+          .toString(16)
+          .substring(1);
+    }
+    return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+      s4() + '-' + s4() + s4() + s4();
+}
+
 function prepareResponse(val) {
     console.log("response", val);
     var debugJSON = JSON.stringify(val, undefined, 2),
